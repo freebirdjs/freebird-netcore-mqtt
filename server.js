e24967af1da3107dd1cd441c67078bb303413534
Simen Li <simenkid@gmail.com>
2016-07-20 14:24:54 +0800

    server prototype ok

--- server.js
+++ server.js
@@ -1,29 +1,44 @@
-var netcore = require('./index.js');
+var netcore = require('./index.js')();
 
 var shepherd = netcore._controller;
 
 netcore.start(function (err, res) {
-    console.log(netcore);
+    console.log('started');
     if (err) console.log(err);
-});
-
-shepherd.on('ready', function () {
-    console.log('shepherd ready');
-    //setTimeout(function () {
-        shepherd.permitJoin(20);
-    //}, 2000);
-    //console.log(shepherd);
-    shepherd.on('priphDisconnected', function (c) {
-        console.log('some one disconnected');
-        console.log(c.id);
-        // var n = shepherd.find(c.id);
-        // console.log(n.status);
+    netcore.permitJoin(180, function () {
+        console.log('permiJoin');
     });
 });
 
-shepherd.on('ind:status', function (qnode, status) {
-    console.log('ind:status');
-    console.log(qnode.status);
-    console.log(status);
-});
+netcore.onReady = function () {
+    console.log('server ready11');
+};
+
+netcore.onDevIncoming = function (dev) {
+    console.log(dev);
+};
+
+netcore.onDevIncoming = function (dev) {
+    console.log(dev);
+};
+
+// shepherd.on('ready', function () {
+//     console.log('shepherd ready');
+//     //setTimeout(function () {
+//         shepherd.permitJoin(20);
+//     //}, 2000);
+//     //console.log(shepherd);
+//     shepherd.on('priphDisconnected', function (c) {
+//         console.log('some one disconnected');
+//         console.log(c.id);
+//         // var n = shepherd.find(c.id);
+//         // console.log(n.status);
+//     });
+// });
+
+// shepherd.on('ind:status', function (qnode, status) {
+//     console.log('ind:status');
+//     console.log(qnode.status);
+//     console.log(status);
+// });
 
