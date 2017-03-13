c4861c7789aca08f6ee5e4ca9ddd4c2a85bbc864
Simen Li <simenkid@gmail.com>
2017-03-13 17:44:08 +0800

    Run tests

--- test/netcore.test.js
+++ test/netcore.test.js
@@ -32,11 +32,12 @@ so.init('temperature', 1, {    // oid = 'humidity', iid = 1
     units: 'cel'
 });
 
-var qnode = new MqttNode('my_foo_client_id', so);
 
 describe('qnode ready', function() {
     it('should ready', function (done) {
-        qnode.on('ready', function () {
+        var xnode = new MqttNode('my_x_client_id', so);
+
+        xnode.on('ready', function () {
             done();
         });
     });
@@ -45,6 +46,8 @@ describe('qnode ready', function() {
 /***********************************************************************/
 /*** Create Netcore                                                  ***/
 /***********************************************************************/
+var qnode = new MqttNode('my_foo_client_id', so);
+
 var mqttCore = require('../index')('netcore-test'),
     controller = mqttCore._controller,
     FBird = require('freebird'),
@@ -52,34 +55,70 @@ var mqttCore = require('../index')('netcore-test'),
 
 var freebird = new FBird([ mqttCore ]);
 
-describe('start freebird', function() {
+describe('start freebird and qnodes come in', function() {
     it('should start', function (done) {
         this.timeout(10000);
         freebird.start(function (err) {
             if (!err) {
-                freebird.permitJoin(180);
-                qnode.connect('mqtt://0.0.0.0');
+                freebird.permitJoin(180);   // open for device joining
                 done();
             }
         });
     });
 
-    // it('should connected', function (done) {
-    //     qnode.on('login', function () {
-    //         setTimeout(function () {
-    //             done();
-    //         }, 2000);
-            
-    //     });
-    //     qnode.connect('mqtt://0.0.0.0');
-    // });
+    it('should connected', function (done) {
+        this.timeout(3000);
+        qnode.connect('mqtt://127.0.0.1');
+
+        qnode.on('login', function () {
+            setTimeout(function () {
+                done();
+            }, 600);   // leave a little time for gadget incoming
+        });
+    });
+
+    it('should have the device', function () {
+        var dev = freebird.findByNet('device', 'netcore-test', '00:11:22:aa:bb:cc/my_foo_client_id');
+
+        expect(dev.isRegistered()).to.be.equal(true);
+        expect(dev.isEnabled()).to.be.equal(true);
+        expect(dev.get('permAddr')).to.be.equal('00:11:22:aa:bb:cc/my_foo_client_id');
+    });
+
+    it('should have the gadget temperature/1', function () {
+        var gad = freebird.findByNet('gadget', 'netcore-test', '00:11:22:aa:bb:cc/my_foo_client_id', 'temperature/1');
+
+        expect(gad.isRegistered()).to.be.equal(true);
+        expect(gad.isEnabled()).to.be.equal(true);
+        expect(gad.get('permAddr')).to.be.equal('00:11:22:aa:bb:cc/my_foo_client_id');
+    });
+
+    it('should have the gadget humidity/0', function () {
+        var gad = freebird.findByNet('gadget', 'netcore-test', '00:11:22:aa:bb:cc/my_foo_client_id', 'humidity/0');
+
+        expect(gad.isRegistered()).to.be.equal(true);
+        expect(gad.isEnabled()).to.be.equal(true);
+        expect(gad.get('permAddr')).to.be.equal('00:11:22:aa:bb:cc/my_foo_client_id');
+    });
+
+    it('should have the gadget humidity/1', function () {
+        var gad = freebird.findByNet('gadget', 'netcore-test', '00:11:22:aa:bb:cc/my_foo_client_id', 'humidity/1');
+
+        expect(gad.isRegistered()).to.be.equal(true);
+        expect(gad.isEnabled()).to.be.equal(true);
+        expect(gad.get('permAddr')).to.be.equal('00:11:22:aa:bb:cc/my_foo_client_id');
+    });
+
+    it('another qnode connected', function (done) {
+        this.timeout(3000);
+        var qnode2 = new MqttNode('foo_id', so);
+
+        qnode2.connect('mqtt://127.0.0.1');
+
+        qnode2.on('login', function () {
+            setTimeout(function () {
+                done();
+            }, 600);   // leave a little time for gadget incoming
+        });
+    });
 });
-
-// var controller = nc._controller,
-//     dev = fbBase.createDevice(nc, rawDev),
-//     gad = fbBase.createGadget(dev, '0xcc00.0xcc07.28', rawGad);
-
-// var invokeCbNextTick = function (cb) {
-//     setImmediate(cb, null);
-// };
-
